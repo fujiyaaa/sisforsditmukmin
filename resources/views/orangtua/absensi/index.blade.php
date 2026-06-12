f83cb3209145c42b8efbbefaacab331c5313c251
Steven Marchy Simanjuntak <stevensmjtk17@gmail.com>
2026-06-12 18:24:16 +0700

    Mengembalikan UI saat Asesmen 2

--- resources/views/orangtua/absensi/index.blade.php
+++ resources/views/orangtua/absensi/index.blade.php
@@ -373,7 +373,7 @@
                             <td class="px-6 py-5 whitespace-nowrap">
 
                                 @if ($absensi->keterlambatan && $absensi->keterlambatan > 0)
-                                    <span class="inline-flex items-center justify-center bg-red-50 text-red-700 border border-red-100 px-4 py-2 rounded-2xl text-sm font-semibold">
+                                    <span class="inline-flex items-center justify-center bg-yellow-50 text-yellow-700 border border-yellow-100 px-4 py-2 rounded-2xl text-sm font-semibold">
                                         {{ $absensi->keterlambatan }} menit
                                     </span>
                                 @else
